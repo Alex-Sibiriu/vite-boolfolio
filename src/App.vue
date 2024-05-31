8c59e6aa93d049a844daef7d97bd778c8aa0a3ee
Alex-Sibiriu <alexsibiriu@protonmail.com>
2024-05-31 14:03:16 +0200

    [add] Api result on Console

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,31 @@
 <script>
+
+import axios from 'axios'
+import { store } from './data/store';
+
 export default {
-  
+  data() {
+    return {
+      projects: []
+    }
+  },
+
+  methods: {
+    getApi() {
+      axios.get(store.apiUrl)
+      .then(response => {
+        this.projects = response.data;
+        console.log(this.projects);
+      })
+      .catch( error => {
+        console.log(error.message);
+      })
+    }
+  },
+
+  mounted() {
+    this.getApi()
+  },
 }
 </script>
 
